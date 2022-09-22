08d684f01582b0c8fbfbcd3ccefa7c17edb8ae36
AlexeyBolt <boltava.av@gmail.com>
2022-09-23 01:55:43 +0300

    Решил все задачи

--- homework_3/ex_19/Program.cs
+++ homework_3/ex_19/Program.cs
@@ -1,6 +1,37 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число");
-string input = (Console.ReadLine()!);
+﻿/* Варинат Google
+Console.Clear();
+Console.WriteLine("Введите пятизначное число : ");
+ string input = (Console.ReadLine()!);
 
 Console.Write("{0}", (input.ToLower() == (new string(input.ToLower().Reverse().ToArray()))) ? "Палиндром" : "Не палиндром");
-Console.ReadKey();
+Console.ReadKey();  */
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число : ");
+int num = int.Parse(Console.ReadLine()!);
+if(num<0)
+{
+    num=num*-1;
+}
+if(num >= 10000 && num <= 99999)
+{
+    int ch1 = num/1000;
+    int end2 = num%100/10;
+    int end1 = num%10;
+
+    if(ch1 == (end1 * 10 + end2))
+    {
+        Console.WriteLine("Это палиндром"); 
+    }
+
+    else 
+    {
+        Console.WriteLine("Это не палиндром");
+    }
+}
+ else 
+{
+    Console.WriteLine("Это не пятизначное число");
+}
+
+
--- homework_3/ex_23/Program.cs
+++ homework_3/ex_23/Program.cs
@@ -5,7 +5,8 @@ int num = int.Parse(Console.ReadLine()!);
 int count = 1;
 while (count<=num)
 {
-    Console.Write(count*count*count);
+    //Console.Write(count*count*count);
+    Console.Write(Math.Pow(count,3));
     count+=1;
     if (count <= num)
     Console.Write(",");
