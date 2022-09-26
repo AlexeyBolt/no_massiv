962599d8d692f60e0e2357cbff9ff97f07671e33
AlexeyBolt <boltava.av@gmail.com>
2022-09-26 23:14:43 +0300

    добавил 3й семинар

---
+++ classroom2/ex_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int[] array = new int[] {-4,-8,8,2};
+int lenght = array.Length;
+for ( int i = 0; i<=lenght-1; i++)
+{
+    array[i] = -array[i];
+}
+
+PrintArray(array);
+
+
+void PrintArray (int[] array)
+{
+    int lenght = array.Length;
+    for (int i=0; i< array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
---
+++ classroom2/ex_6/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Clear();
+int[] array = new int[11];
+
+FillArray(array);
+PrintArray(array);
+int[] newArray = Product(array);
+PrintArray(newArray);
+
+int[] Product(int[] arr)
+{
+    int[] newarr;
+    if (arr.Length % 2 == 0)
+    {
+        newarr = new int[arr.Length/2];
+        for (int i = 0; i < arr.Length/2; i++)
+        {
+            newarr[i] = arr[i]*arr[arr.Length - 1- i];
+        }
+    }
+    else
+    {
+        newarr = new int[arr.Length/2 + 1];
+        for (int i = 0; i < arr.Length/2 + 1; i++)
+        {
+            if (i == arr.Length/2)
+                newarr[i] = arr[i];
+            else 
+                newarr[i] = arr[i]*arr[arr.Length - 1- i];
+            
+        }
+    }
+    return newarr;
+}
+
+void FillArray(int[] array)
+{
+    Random randGenegator = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i]);
+        if (i == count - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }    
+}
