8c2979f2b19717d165f638d2b3999489acdc8d13
AlexeyBolt <boltava.av@gmail.com>
2022-09-30 00:16:36 +0300

    добавил классных работ

---
+++ classroom3/ex_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+string Met4(int count, string textinh)
+{
+
+string result = String.Empty; //   string result = "" ;
+for(int i = 0; i < count; i++)
+    {
+        result = result + textinh;
+    }
+    return result;
+}
+string res = Met4(10, "гы");
+Console.WriteLine(res);
+
+string res1 = Met4(10, "xa");
+Console.WriteLine(res1);
