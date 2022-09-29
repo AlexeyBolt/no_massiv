7f6cda338d3e74fa3e070ec79400dd308e20cb10
AlexeyBolt <boltava.av@gmail.com>
2022-09-30 01:29:49 +0300

    добавил домашку

---
+++ homework_4/ex_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/* - введите 2 числа, и возведите число 1 в степень 2го числа/*  */
+
+Console.Clear();
+
+Console.Write("Введите число   : ");
+  int num = int.Parse(Console.ReadLine());
+  Console.Write("Введите степень : ");
+  int n = int.Parse(Console.ReadLine());
+  int vstepeni=1;
+  for(int i=0; i<n; i++) 
+  {
+   vstepeni*=num;
+  }
+  Console.WriteLine(vstepeni);
+  Console.ReadKey();
+  return 0;
---
+++ homework_4/ex_3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+void FillArray(int[] collect)
+    {
+    int length = collect.Length;
+    int index = 0;
+        while(index < length)
+        {
+        collect[index] = new Random().Next(0, 10);
+        index++;
+        }
+
+
+    }
+void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int pos = 0; 
+        while(pos < count)
+        {
+            Console.Write($"{col[pos]} ");
+            pos++;
+        }
+    }
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
