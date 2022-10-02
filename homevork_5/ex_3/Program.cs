eb64201a459dec691547faffd9c2c145273dfba7
AlexeyBolt <boltava.av@gmail.com>
2022-10-03 02:36:54 +0300

    добавил все задачи домашки

---
+++ homevork_5/ex_3/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+void FillArray(int[] numbers)
+    {
+    double length = numbers.Length;
+    int index = 0;
+        while(index < length)
+        {
+        numbers[index] = new Random().Next(1,10);
+        index++;
+        }
+
+
+    }
+void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int pos = 0; 
+        while(pos < count)
+        {
+            Console.Write($"{col[pos]} ");
+            pos++;
+        }
+    }
+int[] numbers = new int[10]; 
+
+   
+
+
+FillArray(numbers);
+PrintArray(numbers);
+Console.WriteLine();
+int min = numbers[0];
+int max = numbers[0];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] < min) {min = numbers[i] ;}
+        if (numbers[i] > max) {max = numbers[i] ;}
+    }
+Console.WriteLine(max - min);
+
+
