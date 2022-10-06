b95e485803669d79e434403172dbf4a2833caea4
AlexeyBolt <boltava.av@gmail.com>
2022-10-06 20:35:57 +0300

    добавил базовую  домашку

---
+++ homework_6/ex_1/Program.cs
@@ -0,0 +1,55 @@
+﻿int[] array = new int[InputIntNumber()];
+EnterArray(array, 0);
+PrintArray(array);
+SumNull(array);
+
+int InputIntNumber()
+{
+    while (true)
+    {
+        Console.Write("Ведите количество элементов: ");
+        int number = int.Parse(Console.ReadLine() ?? "0");
+        return number;
+    }
+}
+
+void EnterArray(int[] array, int i)
+{
+
+    if (i < array.Length)
+    {
+        Console.Write($"Введите {i+1} эл-т массива: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+        i++;
+        EnterArray(array, i);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i]);
+        if (i == length - 1)
+            Console.WriteLine("]");
+        else
+            Console.Write(", ");
+    }
+}
+
+void SumNull(int[] array)
+{
+    Random rand = new Random();
+    int length = array.Length;
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Введено {count} чисел > 0");
+}
---
+++ homework_6/ex_2/Program.cs
@@ -0,0 +1,47 @@
+﻿double[,] array = new Double[2, 2];
+FillArray(array);
+PrintArray(array);
+CrossPoint(array);
+
+
+void PrintArray(double[,] matrixArray)
+{
+    int row = matrixArray.GetLength(0);
+    int columns = matrixArray.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{matrixArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matrixArray)
+{
+    int rows = matrixArray.GetLength(0);
+    int columns = matrixArray.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        Console.WriteLine($"Числа из ряда {i+1}.");
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"Столбец {j+1}: ");
+            matrixArray[i, j] = int.Parse(Console.ReadLine() ?? "0");
+        }
+    }
+}
+
+void CrossPoint(double[,] matrixArray)
+{
+  
+    double x = (matrixArray[0, 1] - matrixArray[0, 0]) / (matrixArray[1, 0] - matrixArray[1, 1]);
+    double yOne = Math.Round(matrixArray[1, 0] * x + matrixArray[0, 0], 2);
+    double yTwo = Math.Round(matrixArray[1, 1] * x + matrixArray[0, 1], 2);
+
+
+    Console.WriteLine($"координаты  пересечения : ({yOne}; {yTwo})");
+
+}
