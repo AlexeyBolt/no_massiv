c0937d0d0b7ce68707ceb2b4afcf28a8da7d8413
AlexeyBolt <boltava.av@gmail.com>
2022-10-11 22:15:00 +0300

    добавил базовую  домашку 7

---
+++ homework_7/ex_3/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] matrix = new int[3, 4];
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(-10, 10);
+        Console.Write($"{matrix[i, j]} ");
+
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int n = 0; n < matrix.GetLength(1); n++)
+{
+    double sum = 0;
+    for (int m = 0; m < matrix.GetLength(0); m++)
+    {
+
+        sum = sum + matrix[m, n];
+
+    }
+
+    Console.Write(Math.Round(sum / matrix.GetLength(0), 1));
+    Console.Write(" ; ");
+
+}
